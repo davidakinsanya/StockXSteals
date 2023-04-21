b498b1d6c62d1508b219c17dce189f35b1567caf
davidakinsanya <davidakinsanya00@gmail.com>
2023-04-21 21:39:14 +0100

    Adding filter Enums

--- app/app/src/main/java/com/stockxsteals/app/model/filter/Currency.kt
+++ app/app/src/main/java/com/stockxsteals/app/model/filter/Currency.kt
@@ -1,10 +1,10 @@
 package com.stockxsteals.app.model.filter
 
-enum class Currency(string: String, symbol: String) {
-  GBP("GBP", "£"),
-  USD("USD", "$"),
-  JPY("JPY", "JPY"),
-  EUR("EUR", "EUR"),
-  CAD("CAD", "CA$"),
-  AUD("AUD", "AU$")
+enum class Currency(symbol: String) {
+  GBP("£"),
+  USD("$"),
+  JPY("JPY"),
+  EUR("EUR"),
+  CAD("CA$"),
+  AUD("AU$")
 }
