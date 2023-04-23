3b404b54bcd15d1388adc8a9b5e0fa639d1e96bd
davidakinsanya <davidakinsanya00@gmail.com>
2023-04-23 22:53:07 +0100

    Refactoring currency

--- app/app/src/main/java/com/stockxsteals/app/model/filter/Currency.kt
+++ app/app/src/main/java/com/stockxsteals/app/model/filter/Currency.kt
@@ -1,10 +1,10 @@
 package com.stockxsteals.app.model.filter
 
-enum class Currency(val type: String, val symbol: String) {
-  GBP("GBP","£"),
-  USD("USD", "$"),
-  JPY("JPY", "¥"),
-  EUR("EUR","€"),
-  CAD("CA$", "CA$"),
-  AUD("AU$", "AU$")
+enum class Currency(val symbol: String) {
+  GBP("£"),
+  USD( "$"),
+  JPY( "¥"),
+  EUR("€"),
+  CAD( "CA$"),
+  AUD( "AU$")
 }
