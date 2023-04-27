77af0ecd5b5293c80e612cbf9209b486af0471a8
davidakinsanya <davidakinsanya00@gmail.com>
2023-04-27 11:28:23 +0100

    Altering the python script to bypass access denied issues on the stockx website

--- app/app/src/main/python/search.py
+++ app/app/src/main/python/search.py
@@ -1,17 +1,20 @@
 from bs4 import BeautifulSoup as bs4
-import requests
-import re
+import cloudscraper
 
 
 def stockx_search(search_title):
+    url = "https://stockx.com/en-gb/search?s={}".format(search_title)
     search_map = {}
-    
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.138 Safari/537.3"}
-    reg_url = "https://stockx.com/en-gb/search?s={}".format(search_title)
 
-    res = requests.get(reg_url, headers=headers).text
 
-    doc = bs4(res, 'html.parser')
+    scraper = cloudscraper.create_scraper(
+        browser={
+        'browser': 'chrome',
+        'platform': 'android',
+        'desktop': False
+        }
+    )
+    doc = bs4(scraper.get(url).text, 'html.parser')
         
     search_res = doc.find('div', {'class', 'loading css-1ouqd68'})
     search_res2 = search_res.find_all('div', {'class', 'css-pnc6ci'})
@@ -20,10 +23,9 @@ def stockx_search(search_title):
     
     for i in range(0, len(search_res2)):
         search_map[img_search[i].img['alt']] = [
-            search_res2[i].a['href'],
+            search_res2[i].a['href'].replace('/en-gb/', ''),
             img_search[i].img['src'] 
-         ] 
+         ]
         
     return search_map
-
-
+   
