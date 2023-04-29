4a50aeb6c6401a250e5b6c88ade90a25c6300d99
davidakinsanya <davidakinsanya00@gmail.com>
2023-04-29 18:20:46 +0100

    Making changes to fit the python with chaquopy

--- app/app/src/main/python/search.py
+++ app/app/src/main/python/search.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup as bs4
 
 def driver():
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.49 Safari/537.36"
-    options = uc.ChromeOptions()
+    options = webdriver.ChromeOptions()
     options.add_argument('--headless=new')
     options.add_argument(f'user-agent={user_agent}')
     options.add_argument("--window-size=1920,1080")
@@ -17,8 +17,9 @@ def driver():
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
-
+    
     return uc.Chrome(
+            browser_executable_path = "/",
             options=options
         )
 
@@ -41,8 +42,5 @@ def stockx_search(search_title):
             search_res2[i].a['href'].replace('/en-gb/', ''),
             img_search[i].img['src'] 
          ]
-        print(img_search[i].img['src'])
         
     return search_map
-
-stockx_search("Nike Air Max 1 Travis Scott")
