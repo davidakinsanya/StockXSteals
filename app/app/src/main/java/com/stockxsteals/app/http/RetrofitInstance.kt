0a4f65d8f686bf6e44a5a2812e25e0f6dafacc06
davidakinsanya <davidakinsanya00@gmail.com>
2023-05-05 00:32:24 +0100

    retrofit tweak

--- app/app/src/main/java/com/stockxsteals/app/http/RetrofitInstance.kt
+++ app/app/src/main/java/com/stockxsteals/app/http/RetrofitInstance.kt
@@ -6,11 +6,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.time.Duration
 
 object RetrofitInstance {
+
   private const val url = "http://192.168.1.127:8080"
-
   private const val search_url = "http://192.168.1.127:5000"
-
   private val OkHttp = OkHttpClient.Builder().callTimeout(Duration.ofSeconds(5L))
+
   val trend: RetrofitHandler by lazy {
     Retrofit.Builder()
       .baseUrl(url)
