7d0ed8d5deabd28273a8680a071354bbd1f219f6
davidakinsanya <davidakinsanya00@gmail.com>
2023-05-22 21:20:25 +0100

    Implementing companion object for Ktor client as well as provide pseudo code for viewmodel use

--- app/app/src/main/java/com/stockxsteals/app/http/ApiService.kt
+++ app/app/src/main/java/com/stockxsteals/app/http/ApiService.kt
@@ -2,6 +2,11 @@ package com.stockxsteals.app.http
 
 import com.stockxsteals.app.model.dto.Product
 import com.stockxsteals.app.model.dto.Trend
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
 
 interface ApiService {
 
@@ -11,4 +16,31 @@ interface ApiService {
 
   suspend fun searchProduct(query: String, currency: String, country: String): Product
 
+  companion object {
+    fun create(): ApiService {
+    return ApiServiceImpl(
+      client = HttpClient(Android) {
+        install(Logging) {
+          level = LogLevel.ALL
+        }
+        install(JsonFeature) {
+          serializer = KotlinxSerializer()
+        }
+      }
+    )
+    }
+  }
 }
+
+
+/**
+ * service = ApiService.create()
+ * data = produceState<T>(
+ * initialValue = emptyList()
+ * producer = {
+ * value = httpRequest
+ * }
+ * )
+ *
+ * data.value is Type T.
+ */
