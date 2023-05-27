ef1b4b761a80dae91119c62bdaac77263081ae20
davidakinsanya <davidakinsanya00@gmail.com>
2023-05-27 12:33:49 +0100

    Removing another dagger dependency in network view model

--- app/app/src/main/java/com/stockxsteals/app/viewmodel/ui/NetworkViewModel.kt
+++ app/app/src/main/java/com/stockxsteals/app/viewmodel/ui/NetworkViewModel.kt
@@ -5,7 +5,6 @@ import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.widget.Toast
 import androidx.lifecycle.ViewModel
-import dagger.hilt.android.internal.Contexts
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -14,7 +13,7 @@ class NetworkViewModel(): ViewModel() {
   suspend fun checkConnection(context: Context): Boolean {
     return withContext(Dispatchers.IO) {
       val connectivityManager =
-        Contexts.getApplication(context.applicationContext)
+        context.applicationContext
           .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
       val capability = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
       return@withContext capability?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
